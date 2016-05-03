9f4eca91541352ca84b2c89a5fd723a1e35c1079
Okojie Osemudiame Charles <c.charlesworld@gmail.com>
2016-05-04 00:36:12 +0300

    more styling + articles

--- public/articles.js
+++ public/articles.js
@@ -3,7 +3,9 @@ var app = angular.module('articles', [])
 			$scope.init = function(value){
 				$http.get('http://api.nytimes.com/svc/topstories/v1/'+value+'.json?api-key=61feada1b5b912e1a54773b22b5eaf60:11:75164318').
 		       	 success(function(data) {
-		       	 		$scope.mainResult = data.results[1];
+		       	 		  $scope.mainResult = data.results[1];	
+		       	 		  $scope._mainImage = data.results[0].multimedia[4];
+		       	 		  
 		       	 	      var subdata = [];
 		       	 	      var result = 0;
 		       	 	      $scope._subsections = [];
@@ -12,25 +14,38 @@ var app = angular.module('articles', [])
 		       	 	      		subdata.push(result);
 		       	 	      	}
 		       	 	    	for(var x = 0; _subsection_iterator < subdata.length; _subsection_iterator++)
-		       	 	    	{
+		       	 	    	{	
+		       	 	    		//check to see if images for subsection exist then push into array
 		       	 	    		if(data.results[_subsection_iterator].multimedia !== ""){
 		       	 	    		$scope._subsections.push(data.results[_subsection_iterator]);
 		       	 	    		}
 
 		       	 	    	}
 		       	 				       	 	      
-		       	 	  });       	 	    
+		       	 	  }).
+		       	      //check if api call returns null
+		       	 	   error(function (data, status) {
+					      if (status === 404) {
+					        $scope.error = 'API not available';
+					      } else {
+					        $scope.error = 'Error: ' + status;
+					      }
+					      console.log($scope.error)
+					    });     	 	    
 					}
+					//directive: checks if navigation li elemets are clicked
 				}).directive('randomElements', function() {
 				  return {
 				    link: function(scope, element, attr) {
-
 				      element.on('click', function() {
-				      	scope.init(attr.info);
+				      	scope.init(attr.info); // gets the info attribute value
+				      	//element.css('background-color', 'black');   
 				     });
 				      element.ready(function() {
-				      	scope.init("home");
+				      	scope.init("home"); // default value on page loads before use selects another category
 				     });
+				     //element.css('background-color', '');   
+
 			     }
 			  };
 			})
---
+++ public/jq.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('._homelink').css("background-color", "red");
+	  $(".link").click(function() {
+       $(this).css("background-color", "red");
+       $(this).css("color", "white");
+
+       	$('._homelink').css("background-color", "");
+        	$('.link').not(this).each(function(){
+        	$(this).css("background-color", "");
+        });
+
+   		});
+
+	  $("._homelink").click(function(){
+	  	 $(this).css("background-color", "red");
+	  	 $(this).css("color", "white");
+	  	 $('.link').not(this).each(function(){
+        	$(this).css("background-color", "");
+        });
+	  })
+});
