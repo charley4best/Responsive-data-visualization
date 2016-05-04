835607a68c5ba86737f56aafb793362a647caf2d
Okojie Osemudiame Charles <c.charlesworld@gmail.com>
2016-05-04 10:03:42 +0300

    edited navbar

--- public/jq.js
+++ public/jq.js
@@ -18,4 +18,10 @@ $(document).ready(function() {
         	$(this).css("background-color", "");
         });
 	  })
+
+
+    $('.nav li a').on('click', function(){
+    $('.btn-navbar').click(); //bootstrap 2.x
+    $('.navbar-toggle').click() //bootstrap 3.x by Richard
+});
 });
